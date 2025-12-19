0e6ec53f020f9588ecfd0b36b7c6343550b83f15
karimalh <karimhaffar97@gmail.com>
2025-12-19 09:51:42 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: "/wesbsite-test/", // <-- CHANGE to your repo name EXACTLY
+  server: { host: "::", port: 8080 },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 }));
