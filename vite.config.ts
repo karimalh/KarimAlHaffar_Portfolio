b50f465f9088de50f622bde7a1b621b2dd2cd2f3
karimalh <karimhaffar97@gmail.com>
2025-12-29 11:26:40 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/wesbsite-test/", // <-- CHANGE to your repo name EXACTLY
+  base: "/KarimAlHaffar_Portfolio/", // <-- CHANGE to your repo name EXACTLY
   server: { host: "::", port: 8080 },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
