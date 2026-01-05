f6fc2575d3d9510524349bae08a07fc158c27f9d
karimalh <karimhaffar97@gmail.com>
2026-01-05 06:42:31 -0500

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -91,7 +91,7 @@ const Projects = () => {
             </p>
           </div>
 
-          <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-10">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
             {projects.map((project, index) => (
               <div 
                 key={index} 
